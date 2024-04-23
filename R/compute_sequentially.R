e1c3635891533d384473a00c4ca3143d321038d7
Øystein Sørensen <oystein_sorensen@hotmail.com>
2024-04-23 15:31:01 +0200

    adding example data

--- R/compute_sequentially.R
+++ R/compute_sequentially.R
@@ -1,10 +1,34 @@
 #' Compute the Bayesian Mallows model sequentially
 #'
+#'
+#' @param data A dataframe containing partial rankings or pairwise preferences.
+#'   If `data` contains complete or partial rankings, it must have the following
+#'   columns:
+#'
+#' \itemize{
+#' \item `timepoint`: a numeric vector denoting the timepoint, starting at 1.
+#' \item `user`: a vector identifying the user.
+#' \item `item1`: ranking of item 1.
+#' \item `item2`: ranking of item 2.
+#' \item etc.
+#' }
+#'
+#' If data contains pairwise preferences, it must have the following structure:
+#'
+#' \itemize{
+#' \item `timepoint`: a numeric vector denoting the timepoint, starting at 1.
+#' \item `user`: a vector identifying the user.
+#' \item `top_item`: identifier for the preferred item.
+#' \item `bottom_item`: identifier for the dispreferred item.
+#' \item etc.
+#' }
+#'
+#'
 #' @return An object
 #' @export
 #'
 #' @examples
 #' compute_sequentially()
-compute_sequentially <- function(){
-  1
+compute_sequentially <- function(data = NULL){
+
 }
---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Simulated Data
+#'
+#' @format ## `complete_rankings`
+#' A data frame with 100 rows and 7 columns:
+#' \describe{
+#'   \item{timepoint}{Timepoint}
+#'   \item{user}{User id}
+#'   \item{item1, item2, item3, item4, item5}{Ranking given to the item.}
+#' }
+"complete_rankings"
+
+#' Simulated Data with Missing Values
+#'
+#' @format ## `partial_rankings`
+#' A data frame with 100 rows and 7 columns:
+#' \describe{
+#'   \item{timepoint}{Timepoint}
+#'   \item{user}{User id}
+#'   \item{item1, item2, item3, item4, item5}{Ranking given to the item.}
+#' }
+"partial_rankings"
