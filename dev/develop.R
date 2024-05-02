90a09490858e5aa16ea444b25be8e7f4d160d665
Øystein Sørensen <oystein_sorensen@hotmail.com>
2024-05-02 10:26:43 +0200

    all resampling schemes working for pairwise preferences

--- dev/develop.R
+++ dev/develop.R
@@ -1,8 +1,8 @@
 devtools::load_all()
 data <- pairwise_preferences
 hyperparameters <- set_hyperparameters(n_items = 5)
-smc_options <- set_smc_options(n_particles = 50, n_particle_filters = 20, verbose = TRUE)
+smc_options <- set_smc_options(n_particles = 500, n_particle_filters = 100, verbose = TRUE, resampler = "residual")
 ret <- compute_sequentially(data, hyperparameters, smc_options)
 
 plot(ret$alpha_trace[1,])
-
+hist(ret$alpha)
