7dc3b60e6acc0d4ac0299c98e170aa5d0ec0e57e
Øystein Sørensen <oystein_sorensen@hotmail.com>
2024-05-08 17:50:24 +0200

    removed mixtures data, awaiting something better

--- R/data.R
+++ R/data.R
@@ -20,18 +20,6 @@
 #' }
 "complete_rankings_heterogeneous"
 
-
-#' Simulated Data
-#'
-#' @format ## `complete_rankings_mixtures`
-#' A data frame with 1000 rows and 7 columns:
-#' \describe{
-#'   \item{timepoint}{Timepoint}
-#'   \item{user}{User id}
-#'   \item{item1, item2, item3, item4, item5}{Ranking given to the item.}
-#' }
-"complete_rankings_mixtures"
-
 #' Simulated Data with Missing Values
 #'
 #' @format ## `partial_rankings`
@@ -65,17 +53,6 @@
 #' }
 "partial_rankings_updated"
 
-#' Simulated Data with Missing Values
-#'
-#' @format ## `partial_rankings_mixtures`
-#' A data frame with 1000 rows and 7 columns:
-#' \describe{
-#'   \item{timepoint}{Timepoint}
-#'   \item{user}{User id}
-#'   \item{item1, item2, item3, item4, item5}{Ranking given to the item.}
-#' }
-"partial_rankings_mixtures"
-
 #' Simulated Data with Pairwise Preferences
 #'
 #' @format ## `pairwise_preferences`
@@ -97,15 +74,3 @@
 #'   \item{bottom_item}{Item disfavored in the comparison.}
 #' }
 "pairwise_preferences_heterogeneous"
-
-#' Simulated Data with Pairwise Preferences
-#'
-#' @format ## `pairwise_preferences_mixtures`
-#' A data frame with 4000 rows and 4 columns:
-#' \describe{
-#'   \item{timepoint}{Timepoint}
-#'   \item{user}{User id}
-#'   \item{top_item}{Item preferred in the comparison.}
-#'   \item{bottom_item}{Item disfavored in the comparison.}
-#' }
-"pairwise_preferences_mixtures"
