b690202018000847d1af2cd9aa78fe9986145f42
Øystein Sørensen <oystein_sorensen@hotmail.com>
2024-05-08 18:29:09 +0200

    partial dataset

--- tests/testthat/test-compute_sequentially_partial.R
+++ tests/testthat/test-compute_sequentially_partial.R
@@ -5,8 +5,8 @@ test_that("compute_sequentially works with partial data", {
     hyperparameters = set_hyperparameters(n_items = 5),
     smc_options = set_smc_options(n_particles = 100)
   )
-  expect_gt(mean(mod$alpha), .14)
-  expect_lt(mean(mod$alpha), .26)
+  expect_gt(mean(mod$alpha), .9)
+  expect_lt(mean(mod$alpha), 1.1)
 
   set.seed(2)
   mod <- compute_sequentially(
@@ -14,8 +14,8 @@ test_that("compute_sequentially works with partial data", {
     hyperparameters = set_hyperparameters(n_items = 5),
     smc_options = set_smc_options(n_particles = 100, resampler = "residual")
   )
-  expect_gt(mean(mod$alpha), .14)
-  expect_lt(mean(mod$alpha), .26)
+  expect_gt(mean(mod$alpha), .9)
+  expect_lt(mean(mod$alpha), 1.1)
 
   set.seed(2)
   mod <- compute_sequentially(
@@ -23,8 +23,8 @@ test_that("compute_sequentially works with partial data", {
     hyperparameters = set_hyperparameters(n_items = 5),
     smc_options = set_smc_options(n_particles = 100, resampler = "stratified")
   )
-  expect_gt(mean(mod$alpha), .14)
-  expect_lt(mean(mod$alpha), .26)
+  expect_gt(mean(mod$alpha), .9)
+  expect_lt(mean(mod$alpha), 1.1)
 
   set.seed(2)
   mod <- compute_sequentially(
@@ -32,6 +32,6 @@ test_that("compute_sequentially works with partial data", {
     hyperparameters = set_hyperparameters(n_items = 5),
     smc_options = set_smc_options(n_particles = 100, resampler = "systematic")
   )
-  expect_gt(mean(mod$alpha), .14)
-  expect_lt(mean(mod$alpha), .26)
+  expect_gt(mean(mod$alpha), .9)
+  expect_lt(mean(mod$alpha), 1.1)
 })
