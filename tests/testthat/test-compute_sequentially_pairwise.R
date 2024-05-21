bb144e11d5749230627af98d6e2a370a28342387
Øystein Sørensen <oystein_sorensen@hotmail.com>
2024-05-21 09:07:16 +0200

    added a pairwise test to be run

--- tests/testthat/test-compute_sequentially_pairwise.R
+++ tests/testthat/test-compute_sequentially_pairwise.R
@@ -1,38 +1,12 @@
 test_that("compute_sequentially works with pairwise preference data", {
-  skip()
-  set.seed(2)
-  mod <- compute_sequentially(
-    pairwise_preferences,
-    hyperparameters = set_hyperparameters(n_items = 5),
-    smc_options = set_smc_options(n_particles = 200, n_particle_filters = 10, verbose = TRUE)
-  )
-  expect_gt(mean(mod$alpha), .14)
-  expect_lt(mean(mod$alpha), .26)
 
   set.seed(2)
   mod <- compute_sequentially(
-    pairwise_preferences,
+    subset(pairwise_preferences, timepoint < 50),
     hyperparameters = set_hyperparameters(n_items = 5),
     smc_options = set_smc_options(n_particles = 200, resampler = "residual")
   )
-  expect_gt(mean(mod$alpha), .14)
-  expect_lt(mean(mod$alpha), .26)
+  expect_gt(mean(mod$alpha), .75)
+  expect_lt(mean(mod$alpha), 1.25)
 
-  set.seed(2)
-  mod <- compute_sequentially(
-    pairwise_preferences,
-    hyperparameters = set_hyperparameters(n_items = 5),
-    smc_options = set_smc_options(n_particles = 1000, n_particle_filters = 100, resampler = "stratified")
-  )
-  expect_gt(mean(mod$alpha), .14)
-  expect_lt(mean(mod$alpha), .26)
-
-  set.seed(2)
-  mod <- compute_sequentially(
-    pairwise_preferences,
-    hyperparameters = set_hyperparameters(n_items = 5),
-    smc_options = set_smc_options(n_particles = 1000, n_particle_filters = 100, resampler = "systematic")
-  )
-  expect_gt(mean(mod$alpha), .14)
-  expect_lt(mean(mod$alpha), .26)
 })
