b316c7e5aab24fd1a7c846e4c9713c6afce1c588
Øystein Sørensen <oystein_sorensen@hotmail.com>
2024-10-10 13:59:26 +0200

    updating rejuvenation calculation

--- tests/testthat/test-compute_sequentially_partial_updated.R
+++ tests/testthat/test-compute_sequentially_partial_updated.R
@@ -1,21 +1,21 @@
-# dd1 <- complete_rankings[1:20, ]
-# dd1[, -(1:2)][dd1[, -(1:2)] > 1] <- NA
-# dd2 <- complete_rankings[1:20, ]
-# dd2[, -(1:2)][dd2[, -(1:2)] > 2] <- NA
+# dd1 <- complete_rankings[1:30, ]
+# dd1[, -(1:2)][dd1[, -(1:2)] > 2] <- NA
+# dd2 <- complete_rankings[1:30, ]
+# #dd2[, -(1:2)][dd2[, -(1:2)] > 3] <- NA
 # dd <- rbind(dd1, dd2)
 #
 # dd$timepoint <- seq_along(dd$timepoint)
-
-test_that("compute_sequentially works with new and updated users", {
-  # set.seed(2)
-  # mod <- compute_sequentially(
-  #   dd,
-  #   hyperparameters = set_hyperparameters(n_items = 5),
-  #   smc_options = set_smc_options(
-  #     n_particles = 1000, n_particle_filters = 1, verbose = TRUE,
-  #     max_particle_filters = 1000, max_rejuvenation_steps = 1)
-  # )
-  # alpha_hat <- weighted.mean(x = as.numeric(mod$alpha), w = mod$importance_weights)
-  # expect_gt(alpha_hat, .94)
-  # expect_lt(alpha_hat, 1.1)
-})
+#
+# test_that("compute_sequentially works with new and updated users", {
+#   set.seed(2)
+#   mod <- compute_sequentially(
+#     dd,
+#     hyperparameters = set_hyperparameters(n_items = 5),
+#     smc_options = set_smc_options(
+#       n_particles = 300, n_particle_filters = 20, verbose = TRUE,
+#       max_particle_filters = 300, max_rejuvenation_steps = 3)
+#   )
+#   alpha_hat <- weighted.mean(x = as.numeric(mod$alpha), w = mod$importance_weights)
+#   expect_gt(alpha_hat, .94)
+#   expect_lt(alpha_hat, 1.1)
+# })
