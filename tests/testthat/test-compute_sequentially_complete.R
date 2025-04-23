913f3ec316549ffbfda8c7551b357bb38c7e4035
Øystein Sørensen <oystein_sorensen@hotmail.com>
2025-04-23 11:24:37 +0200

    closes #19

--- tests/testthat/test-compute_sequentially_complete.R
+++ tests/testthat/test-compute_sequentially_complete.R
@@ -5,6 +5,7 @@ test_that("compute_sequentially works with complete data", {
     hyperparameters = set_hyperparameters(n_items = 5),
     smc_options = set_smc_options(n_particles = 100, n_particle_filters = 1)
   )
+  expect_s3_class(mod, "BayesMallowsSMC2")
   alpha_hat <- weighted.mean(x = as.numeric(mod$alpha), w = mod$importance_weights)
   expect_gt(alpha_hat, .99)
   expect_lt(alpha_hat, 1.05)
